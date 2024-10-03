f2a828ff86b1140e5739aeb1e7daf6fbab4c8dac
mchenaop2 <mchenaop2@eafit.edu.co>
2024-10-03 09:55:54 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title("Tablero")
+
+# Add canvas component
+# Specify canvas parameters in application
+drawing_mode = "freedraw"
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+stroke_color = '#FFFFFF' # Set background color to white
+bg_color = '#000000'
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=200,
+    width=200,
+    key="canvas",
+)
