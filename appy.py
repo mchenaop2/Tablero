9ed29c4cae1442d36b6dddefb04c6def6bd44fa5
mchenaop2 <mchenaop2@eafit.edu.co>
2024-10-03 10:15:56 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -3,20 +3,17 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(page_title='Tablero', layout='wide')
-st.title('Reconocimiento dibujos a mano')
+st.title('Reconocimiento de Dígitos escritos a mano')
 
 
+# Add canvas component
+# Specify canvas parameters in application
 drawing_mode = "freedraw"
 stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
-stroke_color = st.color_picker("Pick A Color", "#00f900")
+stroke_color = '#FFFFFF' # Set background color to white
 bg_color = '#000000'
 
-option = st.selectbox(
-    "freedraw",
-    ("line", "rect", "circle", "transform", "polygon", "point"),
-)
-
-
+# Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
@@ -24,5 +21,5 @@ canvas_result = st_canvas(
     background_color=bg_color,
     height=200,
     width=200,
-    key="canvas",
+    key="canvas",
 )
